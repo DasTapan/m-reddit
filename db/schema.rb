e5ca08ccd4b5b8a090de78df38a531c81ac2ff16
DasTapan <tapands.das@gmail.com>
2024-08-02 14:18:39 +0530

    Run unique index migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_02_082341) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_02_084625) do
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.integer "user_id", null: false
@@ -35,6 +35,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_02_082341) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "comments", "posts"
