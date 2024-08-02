d6547ef7ab7c626f495acbd86989d63eb7aba23c
DasTapan <tapands.das@gmail.com>
2024-08-02 14:09:43 +0530

    Update migration to remove repetition

--- db/migrate/20240802082341_create_comments.rb
+++ db/migrate/20240802082341_create_comments.rb
@@ -1,8 +1,6 @@
 class CreateComments < ActiveRecord::Migration[7.1]
   def change
     create_table :comments do |t|
-      t.belongs_to :user
-      t.belongs_to :post
       t.text :body
       t.references :user, null: false, foreign_key: true
       t.references :post, null: false, foreign_key: true
